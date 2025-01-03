47ef3d9d24a09100f6900c27e35c316f479f85f8
Syed Ashfaq <36293915+syed-ashfaq@users.noreply.github.com>
2025-01-03 14:36:35 +0530

    Adding feign client to Order service to communicate with inventoryService

--- order-service/src/main/java/com/capgemini/microserives/order/service/OrderService.java
+++ order-service/src/main/java/com/capgemini/microserives/order/service/OrderService.java
@@ -4,6 +4,7 @@ package com.capgemini.microserives.order.service;
 
 
 
+import com.capgemini.microserives.order.client.InventoryClient;
 import com.capgemini.microserives.order.dto.OrderRequest;
 import com.capgemini.microserives.order.entity.Order;
 import com.capgemini.microserives.order.respository.OrderRepository;
@@ -19,14 +20,24 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    private final InventoryClient inventoryClient;
+
     public void placeOrder(OrderRequest orderRequest) {
 
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
             order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+
+
 
     }
 
